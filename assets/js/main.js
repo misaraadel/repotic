59d77d0625f30981a76ab5851d290659e89346a8
misaraadel <41232116+misaraadel@users.noreply.github.com>
2022-09-12 22:26:55 +0200

    Edit some data

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,4 +182,35 @@ $(document).ready(function () {
             }
         }
     });
+
+    $('.owl-partners').owlCarousel({
+        rtl: true,
+        loop:true,
+        margin: 0,
+        nav:false,
+        dots: true,
+        autoplay: true,
+        touchDrag  : true,
+		mouseDrag  : true,
+		autoWidth: false,
+		animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplayTimeout: 6000,
+		smartSpeed: 2000,
+		dragEndSpeed: 2000,
+		slidSpeed: 900,
+        paginationSpeed: 900,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
 });
